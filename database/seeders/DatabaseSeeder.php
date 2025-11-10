58f579c7b9a9012728cc629dd98be558f5f89ac8
Hector Guerra <hector.guerra0123@gmail.com>
2025-11-10 11:28:44 -0300

    Test user se hace en backoffice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-
-        $clientSecret = env('PASSPORT_CLIENT_SECRET', 'wsBa0mp4jwSTYssUGHX5xoqD9IC0X95Gfpg0w3uY');
-        Client::updateOrCreate([
-            'id' => 1
-        ], [
-            'name' => 'Tests',
-            'secret' => $clientSecret,
-            'redirect' => 'http://localhost',
-            'provider' => 'users',
-            'personal_access_client' => false,
-            'password_client' => true,
-            'revoked' => false
-        ]);
     }
 }
